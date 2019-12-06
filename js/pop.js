3052a0d3cc77d8af8da4c32798853da246a7dbf1
yukon12345 <yukon12345@163.com>
2019-12-06 13:49:00 +0800

    说明修改

--- js/pop.js
+++ js/pop.js
@@ -5,6 +5,6 @@ $(function () {
     if (j==null){
         $('#state').html('当前没有复制任何案件信息')
     }else{
-        $('#state').html('当前复制了:'+j.anHao+' 的案件信息')
+        $('#state').html('当前复制了:'+j.anHao+' 的直播信息（'+j.anJianMing+'）')
     }
 })
