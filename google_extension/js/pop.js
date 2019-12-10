7f2f03838a3b7dc5c7f58edb36ef581eef236f4e
yukon12345 <yukon12345@163.com>
2019-12-10 14:56:13 +0800

    完成1.0.2版本更新

--- google_extension/js/pop.js
+++ google_extension/js/pop.js
@@ -8,7 +8,7 @@ var bg = chrome.extension.getBackgroundPage();
 $(function () {
     j=bg.dataJson
     if (j==null){
-        $('#state').html('当前没有复制任何案件信息')
+        $('#state').html('当前没有信息,请到江西直播-发布信息页面-右键复制，然后到中国直播-发布案件页面-右键粘贴，可以自动填写所有信息~<br>微信:yukon12345678')
     }else{
         $('#state').html('当前复制了:'+j.anHao+' 的直播信息（'+j.anJianMing+'）')
     }
